aa3294433cc6142da899d5e42b5826ee21f4edd7
akainic <annekainic@gmail.com>
2015-04-22 08:13:31 -0400

    Add contact form, adjust javascript, change styling

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,15 +1,31 @@
 $(function() {
-  $('.page-link').on('click', function() {
-    var $this = $(this),
-        $destination = $('body').find('section#' + $this.data('destination')),
-        $current = $('.current');
-    $current.removeClass('current');
-    if ($destination.data('order') < $current.data('order')) {
-      $current.addClass('on-right');
+  function changePage(page) {
+    $('body').find('section')
+             .removeClass('current on-right on-left');
+    $(page).prevAll('section')
+                  .addClass('on-left');
+    $(page).nextAll('section')
+                  .addClass('on-right');
+    $(page).addClass('current');
+  }
+
+  function setHeaderLinkColor(page) {
+    $('.page-link').removeClass('dark light');
+    if (page == '#about') {
+      $('.page-link').addClass('dark');
     } else {
-      $current.addClass('on-left');
+      $('.page-link').addClass('light');
     }
-    $destination.removeClass('on-left, on-right')
-                .addClass('current');
+
+  }
+
+  if (window.location.hash != "") {
+    changePage(window.location.hash);
+    setHeaderLinkColor(window.location.hash);
+  }
+
+  $('.page-link').on('click', function() {
+    changePage('#' + $(this).data('destination'));
+    setHeaderLinkColor('#' + $(this).data('destination'));
   });
 });
