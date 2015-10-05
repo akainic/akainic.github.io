55c63dffe51bf2e1e279400bc430d4b075552a57
Anne Kainic <anne@adharmonics.com>
2015-10-04 20:58:28 -0400

    Delete unnecessary js

--- javascripts/main.js
+++ javascripts/main.js
@@ -4,10 +4,6 @@ $(function() {
     menu: '#menu'
   });
 
-  // function setCurrentPage(page) {
-  //   $(page).addClass('active');
-  // }
-
   function setHeaderLinkColor(page) {
     $('.page-link').removeClass('dark light');
     if (page == '#about') {
@@ -18,12 +14,10 @@ $(function() {
   }
 
   if (window.location.hash != "") {
-    // setCurrentPage(window.location.hash);
     setHeaderLinkColor(window.location.hash);
   }
 
   $('.page-link').on('click', function() {
-    // setCurrentPage('#' + $(this).data('destination'));
     setHeaderLinkColor('#' + $(this).data('destination'));
   });
 });
