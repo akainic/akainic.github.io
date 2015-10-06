5363ce8645f612764a2da227a7c022f26eda0987
Anne Kainic <anne@adharmonics.com>
2015-10-05 20:27:23 -0400

    Remove code for changing nav link colors

--- javascripts/main.js
+++ javascripts/main.js
@@ -3,21 +3,4 @@ $(function() {
     anchors: ['landing', 'about', 'work', 'contact'],
     menu: '#menu'
   });
-
-  function setHeaderLinkColor(page) {
-    $('.page-link').removeClass('dark light');
-    if (page == '#about') {
-      $('.page-link').addClass('dark');
-    } else {
-      $('.page-link').addClass('light');
-    }
-  }
-
-  if (window.location.hash != "") {
-    setHeaderLinkColor(window.location.hash);
-  }
-
-  $('.page-link').on('click', function() {
-    setHeaderLinkColor('#' + $(this).data('destination'));
-  });
 });
